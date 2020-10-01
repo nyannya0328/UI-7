da9401108ae7c8fc93c17eb048141b156e164b0b
nyannya0328 <thecuba54@gmail.com>
2020-10-01 20:16:29 +0900

    UI-7

---
+++ UI7/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  UI7
+//
+//  Created by にゃんにゃん丸 on 2020/10/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        
+        NavigationView(){
+            
+            List{
+             
+                ForEach(0..<30){_ in
+
+                    HStack{
+                    Image("image").resizable().frame(width: 40, height: 40)
+                        .cornerRadius(30)
+                    
+                Text("unsplash")
+                        
+                    }
+                }
+                
+                
+            }.navigationBarTitle("UI8")
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UI7/UI7App.swift
@@ -0,0 +1,17 @@
+//
+//  UI7App.swift
+//  UI7
+//
+//  Created by にゃんにゃん丸 on 2020/10/01.
+//
+
+import SwiftUI
+
+@main
+struct UI7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
